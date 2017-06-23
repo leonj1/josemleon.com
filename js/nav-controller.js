955f17f3e894da28bfba6eb4f05477c7c8f7425c
Jose Leon <leonj1@gmail.com>
2017-06-23 19:30:41 -0400

    Initial commit

---
+++ js/nav-controller.js
@@ -0,0 +1,13 @@
+angular.module('navController', [])
+	.controller('nav', function($scope, $state) {
+		$scope.title = 'Determined';
+
+		// returns true if the current router url matches the passed in url
+		// so views can set 'active' on links easily
+		$scope.isUrl = function(url) {
+			if (url === '#') return false;
+			return ('#' + $state.$current.url.source + '/').indexOf(url + '/') === 0;
+		};
+
+		$scope.pages = []
+	});
